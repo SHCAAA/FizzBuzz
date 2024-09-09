fce27ba96b3320fa172a0c3bd27247d18ffe3f58
SH <seungho@SHui-MacBookAir.local>
2024-09-09 18:29:56 -0400

    done Lab1

---
+++ src/Multiples.java
@@ -0,0 +1,6 @@
+public class Multiples {
+    public static void main(String[] args) {
+        System.out.println(467);
+        // there are 333, 200, 66 mutiples of 3, 5, 15 respectively, thus, the answer is 333+200- 66 = 467
+    }
+}
---
+++ src/Reduce.java
@@ -0,0 +1,7 @@
+public class Reduce {
+    public static void main(String[] args) {
+        System.out.println(9);
+
+        //100 -> 50 -> 25 -> 24 -> 12 -> 6 -> 3 -> 2 -> 1 -> 0; total 9 steps.
+    }
+}
